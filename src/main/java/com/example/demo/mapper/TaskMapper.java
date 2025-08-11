aa99513c5fce0961756e6580d425ef9208835c7d
javokhirkhasanov2003 <javokhirkhasanov2003@gmail.com>
2025-08-11 15:40:45 +0500

    Add all

--- src/main/java/com/example/demo/mapper/TaskMapper.java
+++ src/main/java/com/example/demo/mapper/TaskMapper.java
@@ -4,10 +4,11 @@ import com.example.demo.DTOs.TaskCreateDTO;
 import com.example.demo.DTOs.TaskDTO;
 import com.example.demo.model.Task;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
+    TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
     TaskDTO toDto(Task task);
     Task toEntity(TaskCreateDTO taskCreateDTO);
-
 }
